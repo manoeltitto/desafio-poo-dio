3cc6a8dfaa16ae08b11e78754cc8624102287166
manoeltitto <manoeltito@hotmail.com>
2023-04-04 15:41:47 -0300

    commit final do Desafio Java Básico DIO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -11,12 +10,15 @@ public class Main {
       curso1.setCargaHoraria(8);
 
       Curso curso2 = new Curso();
-      curso2.setTitulo("Curos JavaScript");
+      curso2.setTitulo("Curso JavaScript");
       curso2.setDescricao("Descrição do Curso JavaScript");
       curso2.setCargaHoraria(4);
 
-      Mentoria mentoria = new Mentoria();
+      //Conteudo conteudo = new Conteudo(); não se pode instaciar
+      //Realize Polimorfismo
+      //Conteudo conteudo = new Curso();   //Exemplo de Polimorfismo
 
+      Mentoria mentoria = new Mentoria();
       mentoria.setTitulo("Mentoria Java");
       mentoria.setDescricao("Descrição Mentoria Java");
       mentoria.setData(LocalDate.now());
@@ -26,7 +28,38 @@ public class Main {
       System.out.println(curso2);
       System.out.println(mentoria);
 
+    Bootcamp bootcamp = new Bootcamp();
+    bootcamp.setNome("Bootcamp Java Developer");
+    bootcamp.setDescricao("Descricao Bootcampo Java developer");
+    bootcamp.getConteudos().add(curso1);
+    bootcamp.getConteudos().add(curso2);
+    bootcamp.getConteudos().add(mentoria);
 
+    Dev devManoel = new Dev();
+    devManoel.setNome("Manoel");
+    devManoel.inscreverBootcamp(bootcamp);
+    System.out.println("Conteúdos Inscritos" + devManoel.getConteudosInscritos());
+    devManoel.progredir();
+    devManoel.progredir();
+    System.out.println("_");
+
+    System.out.println("Conteúdos Inscritos" + devManoel.getConteudosInscritos());
+      System.out.println("Conteúdos Concluídos" + devManoel.getConteudosConcluidos());
+      System.out.println("XP: "+devManoel.calcularTotalXp());
+
+    Dev devTitto = new Dev();
+    devTitto.setNome("Titto");
+    devTitto.inscreverBootcamp(bootcamp);
+    System.out.println("Conteúdos Inscritos"+ devTitto.getConteudosInscritos());
+    devTitto.progredir();
+    devTitto.progredir();
+    devTitto.progredir();
+    System.out.println("_");
+
+
+    System.out.println("Conteúdos Inscritos" + devTitto.getConteudosInscritos());
+      System.out.println("Conteúdos Concluídos"+ devTitto.getConteudosConcluidos());
+      System.out.println("XP: "+devTitto.calcularTotalXp());
 
     }
 }
